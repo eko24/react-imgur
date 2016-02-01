b844c5c313ae1669d3e687e067092728ddfb90b7
Erik Kotsuba <erik.kotsiuba@udelphi.com>
2016-02-01 16:56:40 +0200

    Initial

---
+++ src/routes.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var IndexRoute = ReactRouter.IndexRoute;
+var Link = ReactRouter.Link;
+
+var Main = require('./components/main')
+
+module.exports = (
+  <Router>
+   <Route path="/" component={Main}/>
+ </Router>
+)
