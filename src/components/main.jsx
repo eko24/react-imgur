54177635a07ab6b5166e8b15d37848004cfcf284
Erik Kotsuba <erik.kotsiuba@udelphi.com>
2016-02-01 17:24:44 +0200

    Added header

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,9 +1,13 @@
 var React = require('react');
+var Header = require('./header');
+
+var renderCount = 0;
 
 module.exports = React.createClass({
   render: function () {
+    console.log(renderCount++);
     return <div>
-      I'm a header.
+      <Header/>
       {this.props.children}
     </div>
   }
