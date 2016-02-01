86db6bae32a8251381d365bbc02890f6420b1e0c
Erik Kotsiuba <eko24ive@gmail.com>
2016-02-01 21:26:49 +0200

    added topic store

---
+++ src/utils/api.jsx
@@ -0,0 +1,15 @@
+var rootUrl = 'https://api.imgur.com/3/';
+var apiKey = '8599e447d97b90d';
+
+module.exports = window.api = {
+  get: function (url) {
+    return fetch(rootUrl+url,{
+      headers: {
+        'Authorization':'Client-ID ' + apiKey
+      }
+    })
+    .then(function(response) {
+      return response.json()
+    });
+  }
+}
